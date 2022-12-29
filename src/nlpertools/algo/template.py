690713fd5004f0c28126eaa6f4bc8349e878de9f
lvzi <zjs20001205@gmail.com>
2022-12-29 15:55:18 +0800

    feat(algo): add algo lc template

---
+++ src/nlpertools/algo/template.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+# @Author  : youshu.Ji
+import heapq
+from collections import defaultdict, deque, Counter
+from typing import List, Optional
+# from sortedcontainers import SortedDict, SortedList
