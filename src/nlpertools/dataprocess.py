b3cbc6cfdbc6569af46d30f969b8ed2a01661aaa
lvzi <zjs20001205@gmail.com>
2023-05-09 09:41:59 +0800

    feat(dataprocess): add common dp pattern

--- src/nlpertools/dataprocess.py
+++ src/nlpertools/dataprocess.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 # @Author  : youshu.Ji
+import re
+
+
+class Pattern:
+    # from where
+    # hlink
+    pattern = re.compile(r"(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]")
+    # emoji
+    pattern_emoji = re.compile(u"[\U00010000-\U0010ffff\\uD800-\\uDBFF\\uDC00-\\uDFFF]")
