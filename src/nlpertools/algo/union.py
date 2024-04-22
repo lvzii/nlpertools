9730ed5449407277fff9c41030104f777b482048
lvzi <zjs20001205@gmail.com>
2024-04-22 09:35:30 +0800

    feat(algo): add union

---
+++ src/nlpertools/algo/union.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+# @Author  : youshu.Ji
+class UnionFind():
+    def __init__(self, num):
+        self.parent = list(range(num))
+
+    def union(self):
+        pass
+
+    def find(self, index):
+        if 1:
+            pass
