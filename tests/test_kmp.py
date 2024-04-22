ecf20293e8a82283ad0ac6c9864710db6bff1b2c
lvzi <zjs20001205@gmail.com>
2024-04-22 09:29:45 +0800

    test(test): add llm memory calc case

--- tests/test_kmp.py
+++ tests/test_kmp.py
@@ -3,15 +3,28 @@
 # @Author  : youshu.Ji
 import pytest as pytest
 
-from src.nlpertools.algo import kmp
-@pytest.mark.parametrize(
-    "main_string, pattern_string, ans",
-    [
-        ("a", "a", 0)
-    ]
-)
+
+@pytest.mark.parametrize("main_string, pattern_string, ans", [("a", "a", 0)])
 def test_kmp_find_after_build(main_string, pattern_string, ans):
+    from src.nlpertools.algo import kmp
 
     res = kmp.find_after_build(main_string, pattern_string)
     assert res == ans
 
+
+def test_ml_calc_llm_train_activation_memory():
+    from src.nlpertools.ml import calc_llm_train_activation_memory
+
+    res = calc_llm_train_activation_memory(
+        model_name="",
+        sequence_length=2048,
+        batch_size=1,
+        hidden_dim=4096,
+        lay_number=28,
+        attention_heads_num=32,
+        gpu_num=1,
+    )
+    print(res, "G")
+
+
+test_ml_calc_llm_train_activation_memory()
