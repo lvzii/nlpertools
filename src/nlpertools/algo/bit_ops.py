166e0172afc0bd21b5ee627b9cd54256975e8305
lvzi <zjs20001205@gmail.com>
2024-04-22 09:35:04 +0800

    feat(bit): add example

--- src/nlpertools/algo/bit_ops.py
+++ src/nlpertools/algo/bit_ops.py
@@ -6,9 +6,23 @@ def foo(num):
 
 
 def foo2(num):
+    """
+    raw: 0 1 2 3 4 5 6 7 8 9
+    res: 0 0 0 2 0 4 4 6 0 8
+    """
     return num & (num - 1)
 
 
 def _lowbit(index: int) -> int:
-    #
+    """
+    raw: 0 1 2 3 4 5 6 7 8 9
+    res: 0 1 2 1 4 1 2 1 8 1
+    """
     return index & -index
+
+if __name__ == '__main__':
+    for i in range(10):
+        print(i, end=" ")
+    print()
+    for i in range(10):
+        print(foo2(i), end=" ")
